f6dbd9c6919aa9fbe9df0b0a900201b281aff4c9
f_ms <hi@imf.ms>
2019-06-15 16:49:11 +0800

    add compiler-plugin 'node convert checker'

---
+++ compiler-plugin-node-convert-checker/src/main/java/ms/imf/redpoint/compiler/plugin/nodeconverter/NodeConverterCheckCompilerPlugin.java
@@ -0,0 +1,44 @@
+package ms.imf.redpoint.compiler.plugin.nodeconverter;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+import ms.imf.redpoint.compiler.plugin.ParsedNodeSchemaHandlePlugin;
+import ms.imf.redpoint.converter.ArgCheckUtil;
+import ms.imf.redpoint.entity.NodeSchema;
+
+/**
+ * node convert checker compiler plugin
+ * @author f_ms
+ */
+public class NodeConverterCheckCompilerPlugin implements ParsedNodeSchemaHandlePlugin {
+
+    @Override
+    public void onParsed(String[] args, List<NodeSchema> nodeSchemas) throws Exception {
+
+        if (args == null
+                || args.length <= 0
+                || args[0] == null
+                || args[0].isEmpty()) {
+            throw new IllegalArgumentException("args can't be empty, I need convertCheckConfigFilePath, please add it into args[0]");
+        }
+
+        String convertCheckConfigFilePath = args[0];
+
+        InputStream convertCheckFileInputStream;
+        try {
+            convertCheckFileInputStream = new FileInputStream(convertCheckConfigFilePath);
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(String.format("args[0]-convertCheckConfigFilePath(%s) not exist, please check file path", convertCheckConfigFilePath), e);
+        }
+
+        try {
+            ArgCheckUtil.checkArg(convertCheckFileInputStream, nodeSchemas);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(String.format("found error on convert config check: %s", e.getMessage()), e);
+        }
+    }
+
+}
