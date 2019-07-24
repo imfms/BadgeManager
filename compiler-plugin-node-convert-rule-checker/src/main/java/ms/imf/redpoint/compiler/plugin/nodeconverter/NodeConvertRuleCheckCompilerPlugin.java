0781e2c6bb07c95565c73280f25ccf50acb81874
f_ms <hi@imf.ms>
2019-07-24 22:41:05 +0800

    NodeConvertRuleCheckPlugin add more accept resource type

--- compiler-plugin-node-convert-rule-checker/src/main/java/ms/imf/redpoint/compiler/plugin/nodeconverter/NodeConvertRuleCheckCompilerPlugin.java
+++ compiler-plugin-node-convert-rule-checker/src/main/java/ms/imf/redpoint/compiler/plugin/nodeconverter/NodeConvertRuleCheckCompilerPlugin.java
@@ -1,20 +1,19 @@
 package ms.imf.redpoint.compiler.plugin.nodeconverter;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import ms.imf.redpoint.compiler.plugin.AptProcessException;
 import ms.imf.redpoint.compiler.plugin.NodeTreeHandlePlugin;
 import ms.imf.redpoint.converter.ConvertRule;
 import ms.imf.redpoint.converter.ConvertRuleChecker;
+import ms.imf.redpoint.util.ResourceHandler;
 
 /**
  * 节点转换规则检查插件，用于对编译期节点转换规则的检查提供支持。fail-fast使错误尽早显现
  *
  * <pre>
  * 所需参数：
- *   参数1：节点转换规则JSON文件路径，JSON格式为toJson(List<{@link ConvertRule}>)
+ *   参数1：节点转换规则JSON资源类型及位置，JSON格式为toJson(List<{@link ConvertRule}>)，支持资源类型参见 {@link ResourceHandler}
  * </pre>
  *
  * @author f_ms
@@ -27,16 +26,23 @@ public class NodeConvertRuleCheckCompilerPlugin implements NodeTreeHandlePlugin 
                 || context.args().length <= 0
                 || context.args()[0] == null
                 || context.args()[0].isEmpty()) {
-            throw new AptProcessException("args can't be empty, I need convertCheckConfigFilePath, please add it into args[0]");
+            throw new AptProcessException("args can't be empty, I need convertCheckConfigResourceStr, please add it into args[0]");
         }
 
-        String convertCheckConfigFilePath = context.args()[0];
+        String resourceStr = context.args()[0];
 
         InputStream convertCheckFileInputStream;
         try {
-            convertCheckFileInputStream = new FileInputStream(convertCheckConfigFilePath);
-        } catch (FileNotFoundException e) {
-            throw new AptProcessException(String.format("args[0]-convertCheckConfigFilePath(%s) not exist, please check file path", convertCheckConfigFilePath), e);
+            convertCheckFileInputStream = ResourceHandler.read(context.processingEnvironment(), resourceStr);
+        } catch (Exception e) {
+            throw new AptProcessException(
+                    String.format(
+                            "found error on read args[0]-convertCheckConfigResource %s: %s",
+                            resourceStr,
+                            e.getMessage()
+                    ),
+                    e
+            );
         }
 
         try {
